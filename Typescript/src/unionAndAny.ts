1db8983d09114e8615c0107fd2ebd3097c7ac156
harishcsx <csx.harish@gmail.com>
2026-01-08 03:08:19 +0530

    unions and any lecture

---
+++ Typescript/src/unionAndAny.ts
@@ -0,0 +1,17 @@
+let subs: number | string = "1M";
+
+// case insensitive 
+let apiRequestStatus: "pending" | "sucess" | "error" = "pending";
+
+const orders = ["10", "20", "28", "42"]
+
+let currentOrder: string | undefined;
+
+for(let order of orders) {
+    if (order === "28") {
+        currentOrder = order;
+        break;
+    }
+}
+
+// any : means i don't care for the type 
